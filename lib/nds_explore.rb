fce5b15ddcb27f7cfc09993872009e0650efadf3
Luke Lin <lukelin1991@yahoo.com>
2019-11-27 20:17:22 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,7 @@ end
 def print_first_directors_movie_titles
   movie_length = 0
   while movie_length < directors_database[0][:movies].length do
-    puts directors_database[0][:movies][movie_length]
+    puts directors_database[0][:movies][:title]
     movie_length += 1
   end
   #puts directors_database[0]
